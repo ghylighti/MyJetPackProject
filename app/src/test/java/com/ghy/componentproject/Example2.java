8695ccaf70731830c5c7d811fb4e9b372237ad41
ghylighti <46590425+ghylighti@users.noreply.github.com>
2020-12-23 22:42:12 +0800

    init

--- app/src/test/java/com/ghy/componentproject/Example2.java
+++ app/src/test/java/com/ghy/componentproject/Example2.java
@@ -6,26 +6,9 @@ public class Example2 {
     @Test
     public void git()
     {
-      test1();
-      test2();
-      test2("test2");
-      test1("test1");
+
     }
 
-    private void test1(String test1) {
-    }
-
-    private void test2() {
-
-        System.out.println("test2");
-    }
-    private void test2(String s) {
-        System.out.println(s);
-        System.out.println(s);
-    }
-    private void test1() {
-        System.out.println("test1");
-    }
 
 
 }
