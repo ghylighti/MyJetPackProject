b41fddc7f9a11b15412f8705ecd7a2e7ab52421e
ghylighti <46590425+ghylighti@users.noreply.github.com>
2020-12-23 23:05:57 +0800

    开发功能3

--- app/src/test/java/com/ghy/componentproject/Example2.java
+++ app/src/test/java/com/ghy/componentproject/Example2.java
@@ -14,6 +14,7 @@ public class Example2 {
         System.out.println("开发功能1");
         System.out.println("开发功能2");
         System.out.println("开发功能3");
+        System.out.println("开发功能模块1完成");
 
     }
 
