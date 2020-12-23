bde7f313b80068fb1332e064e8ebd86d7ac35875
ghylighti <46590425+ghylighti@users.noreply.github.com>
2020-12-23 22:51:06 +0800

    testFun2

--- app/src/test/java/com/ghy/componentproject/Example2.java
+++ app/src/test/java/com/ghy/componentproject/Example2.java
@@ -11,11 +11,17 @@ public class Example2 {
 
     private void fun2() {
         testFun2();
+
     }
 
     private void testFun2() {
         System.out.println("testFun2");
         System.out.println("开发功能模块2.1");
+        bug();
+    }
+
+    private void bug() {
+        System.out.println("bug");
     }
 
 
